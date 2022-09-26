b146d6c300ca5ef80645597d0d2c23e184a6ff7b
Sheri Lu <sherisherilu@gmail.com>
2022-09-26 23:40:56 +0000

    LHL M1 Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+const assertArraysEqual = (array1,array2) => {
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i] || array1.length !== array2.length) {
+            console.log(`🛑🛑🛑 Assertion Failed:  ${array1}  !== ${array2}`);
+            break;
+        } 
+    }
+    console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);;
+};
+
+
+const eqArrays = (array1,array2) => {
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i] || array1.length !== array2.length) {
+            return false;
+            break;
+        } 
+    }
+    return true;
+}
+
+
+const letterPositions = function(sentence) {
+    const results = {};
+    // logic to update results here
+
+    //space doesn't count
+    sentence = sentence.replace(' ','');
+
+    //iterate through the sentence
+    for (let i = 0; i < sentence.length; i++) {
+        if (results[sentence[i]]) {
+            //if the same letter occurs, push its index to "results"
+            results[sentence[i]].push(i);
+        } else {
+            results[sentence[i]] = [i]
+        }
+    }
+    return results;
+  };
+
+//test code below
+
+testCode = letterPositions("hello");
+
+assertArraysEqual(testCode["h"], [0])
+assertArraysEqual(testCode["e"],[1])
+assertArraysEqual(testCode["l"], [2, 3])
+assertArraysEqual(testCode["o"], [4])
+
+
