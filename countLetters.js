1fec5479c40deed504470210fc6cbd93e808c008
Sheri Lu <sherisherilu@gmail.com>
2022-09-26 19:52:04 +0000

    LHL M1 Implement countLetters in Lotide

---
+++ countLetters.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed:  ${actual}  !== ${expected}`);
+    }
+};
+
+const countLetters = (sentence) => {
+    let result = {};
+
+    //space doesn't count
+    sentence = sentence.replace(' ','');
+
+
+    //loop through the letters
+    for (const element of sentence) {
+        if (!result[element]) {
+            result[element] = 1 ;
+        } else {
+            result[element]++ ;
+        }
+    }
+    return result;
+};
+
+
+//test code below
+
+const sentence1 = 'lighthouse vancouver';
+const result1 = countLetters(sentence1);
+
+assertEqual(result1['v'],2);
+assertEqual(result1['z'],undefined);
+assertEqual(result1['g'],1);
+
+
+
+
+
