b4ebf04d61a2fcf1decbd7ad6e687267b1850e4b
Sheri Lu <sherisherilu@gmail.com>
2022-09-28 00:33:42 +0000

    LHL M1 Implement findKeyByValue in Lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed:  ${actual}  !== ${expected}`);
+    }
+};
+
+
+
+const findKeyByValue = (inputObject, value) => {
+
+    let keyInObject = Object.keys(inputObject);
+
+    for (const eachKey of keyInObject) {
+      if (inputObject[eachKey] === value) {
+        return eachKey;
+      } 
+    }
+};
+
+//Test Code
+const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
