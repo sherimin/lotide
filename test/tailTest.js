7a23960a5808fef9d288863886ff430175db1c4a
Sheri Lu <sherisherilu@gmail.com>
2022-10-12 21:58:51 +0000

    LHL Refactor with Mocha&Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,20 @@
 const tail = require('../tail')
+const assert = require('chai').assert;
+
+//Test Code
+
+
+describe("#tail", () => {
+    it("returns [2, 3] for [1, 2, 3]", () => {
+      assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    });
+
+    it("returns ['Lighthouse','Labs'] for ['Hello','Lighthouse','Labs']", () => {
+        assert.deepEqual(tail(['Hello','Lighthouse','Labs']), ['Lighthouse','Labs']);
+      });
+
+
+});
 
 
 
