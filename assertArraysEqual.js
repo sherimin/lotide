47f7558eec31853e08eb3dc5d258bc0cb4d6f245
Sheri Lu <sherisherilu@gmail.com>
2022-10-12 00:23:35 +0000

    Refactor with test files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,11 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = (array1,array2) => {
-    for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i] || array1.length !== array2.length) {
-            console.log(`🛑🛑🛑 Assertion Failed:  ${array1}  !== ${array2}`);
-            break;
-        } 
+    if (eqArrays(array1, array2)) {
+        console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed:  ${array1}  !== ${array2}`);
     }
-    console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);;
 };
 
+module.exports = assertArraysEqual;
