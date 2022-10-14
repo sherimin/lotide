c6a54850aa021220f126a17dcf6e97d491e23689
Sheri Lu <sherisherilu@gmail.com>
2022-10-14 18:53:21 +0000

    refactor

--- map.js
+++ map.js
@@ -1,9 +1,6 @@
 const words = ["ground", "control", "to", "major", "sakura"];
 
 const map = function (array,callback) {
-    //console.log('array: ', array);
-    //console.log('callback: ', callback);
-
     const results = [];
     for (let item of array) {
         results.push(callback(item));
